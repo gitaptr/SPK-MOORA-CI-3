4a3005b774b8a5d278629330fc5b9602bd49fd36
gitaptr <2011523011_gita@student.unand.ac.id>
2025-07-15 06:47:46 +0700

    updatenamafile

--- application/controllers/Sub_kriteria.php
+++ application/controllers/Sub_kriteria.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Sub_kriteria extends CI_Controller
+class Sub_Kriteria extends CI_Controller
 {
 
     public function __construct()
